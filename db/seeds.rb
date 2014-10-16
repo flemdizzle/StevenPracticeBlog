172f2cefbbbd5d7bad37cb51a5a3b9d38c722e9c
flemdizzle <fleminsh@gmail.com>
2014-10-16 09:25:07 -0400

    pushing to github

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,10 @@
 Post.destroy_all
 
 Post.create!([
-      
-   ])
+{title: "This is Blog", body: "Isn't that weird?"},
+{title: "I am a beer", body: "La ti da!"},
+{title: "Welcome to the Jungle", body: "Derp"},
+{title: "I accidentally", body: "The whole thing"},
+{title: "BLOGE", body: "Herp"},
+{title: "Hey what", body: "Is going on?"}
+      ])
