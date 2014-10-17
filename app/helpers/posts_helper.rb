5bb93cdbfa92dba4c14664e5839ebf1ac4935a2b
flemdizzle <fleminsh@gmail.com>
2014-10-17 15:06:23 -0400

    got through a lot of the navbar

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+
+    def social(int, symbol, **options)
+      mask = options.fetch(:mask, nil)
+      if int.to_s.length == 9
+         if mask
+            
+         else
+          string = int.to_s
+          return string[0..2] + symbol + string[3..4] + symbol + string[5..8]
+         end
+      else
+          return "please give a valid social"
+      end
+    end
+
 end
