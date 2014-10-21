96e1d7fa88bd262e5a7f5c79f2d0a247f2746e6b
flemdizzle <fleminsh@gmail.com>
2014-10-21 18:30:35 -0400

    fixed silly bug with broken create method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,7 @@ class UsersController < ApplicationController
   # POST /users
   # POST /users.json
   def create
-    @user = User.new(params)
+    @user = User.new(users_params)
 
     respond_to do |format|
       if @user.save
